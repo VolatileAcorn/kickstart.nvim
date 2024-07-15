84da167546e87aa2df3ffdb1ca41e461f815f867
Tom Pottage <Tom.Pottage@etas.com>
2024-07-15 17:08:33 +0100

    Update color scheme for comments to be green

--- init.lua
+++ init.lua
@@ -713,6 +713,12 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      require('tokyonight').setup {
+        style = 'night',
+        on_colors = function(colors)
+          colors.comment = '#66cf89'
+        end,
+      }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
